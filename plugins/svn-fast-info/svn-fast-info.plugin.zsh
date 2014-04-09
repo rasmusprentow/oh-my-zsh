a550d19a0c46badeef910f07d0cacb966fd727d6
alex <abondoa@gmail.com>
2014-04-09 13:59:41 +0200

    git more weird symbols added

--- plugins/svn-fast-info/svn-fast-info.plugin.zsh
+++ plugins/svn-fast-info/svn-fast-info.plugin.zsh
@@ -24,18 +24,18 @@ function svn_prompt_info() {
       $ZSH_THEME_SVN_PROMPT_SUFFIX \
       $ZSH_PROMPT_BASE_COLOR
   else
-    printf '%s%s%s %s%s%s%s%s%s%s' \
+    printf '%s%s%s%s%s%s%s%s%s%s' \
       $ZSH_PROMPT_BASE_COLOR \
       $ZSH_THEME_SVN_PROMPT_PREFIX \
       \
-      "$(svn_status_info $info)" \
+      r:$(svn_current_revision $info) \
       $ZSH_PROMPT_BASE_COLOR \
       \
       $ZSH_THEME_BRANCH_NAME_COLOR \
       $(svn_current_branch_name $info) \
       $ZSH_PROMPT_BASE_COLOR \
       \
-      r:$(svn_current_revision $info) \
+      "$(svn_status_info $info)" \
       $ZSH_PROMPT_BASE_COLOR \
       \
       $ZSH_THEME_SVN_PROMPT_SUFFIX \
