676e46d23f842015c311a675100dad7a5eecaa37
Rasmus Prentow <prentow@uber.com>
2017-09-12 17:09:09 +0200

    Added work tools

--- plugins/uber/uber.plugin.zsh
+++ plugins/uber/uber.plugin.zsh
@@ -1,8 +1,8 @@
 
 alias ad='arc diff'
+alias ult='cn && make lint && gulp test'
 alias adl='arc diff --reviewers lasse,olerass,#si_ui_team'
 alias adh='arc diff HEAD^1 --reviewers lasse,olerass,#si_ui_team'
-alias grm='git rebase master'
 alias grim='git rebase -i master'
 alias grhs='git reset HEAD^1 --soft'
 alias gaca='git add -A && git commit --amend'
@@ -13,6 +13,10 @@ gcaa() {
 
 alkb() {
   FEATURE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  arc land $* --keep-branch && gcm && gl && gco $FEATURE_BRANCH && grm
+  arc land $* --keep-branch && gcm && gl && gco $FEATURE_BRANCH && grbm
 }
 
+gpim() {
+  FEATURE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  gcm && gl && gco $FEATURE_BRANCH && grbm
+}
