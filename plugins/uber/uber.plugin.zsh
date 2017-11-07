0e752aab7ab7673cf5e1c5d056b6424221ce29fb
Rasmus Prentow <prentow@uber.com>
2017-11-07 09:40:59 +0100

    updated uber tools

--- plugins/uber/uber.plugin.zsh
+++ plugins/uber/uber.plugin.zsh
@@ -6,7 +6,7 @@ alias adh='arc diff HEAD^1 --reviewers lasse,olerass,#si_ui_team'
 alias grim='git rebase -i master'
 alias grhs='git reset HEAD^1 --soft'
 alias gaca='git add -A && git commit --amend'
-
+alias gacan='git add -A && git commit --amend --no-edit'
 gcaa() {
   git add -A && git commit -m "$*"
 }
@@ -16,7 +16,19 @@ alkb() {
   arc land $* --keep-branch && gcm && gl && gco $FEATURE_BRANCH && grbm
 }
 
+gpodb() {
+  git push origin --delete $(git rev-parse --abbrev-ref HEAD)
+}
+
 gpim() {
   FEATURE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
   gcm && gl && gco $FEATURE_BRANCH && grbm
 }
+
+alias uui='cd ~/projects/udeploy-ui'
+alias uua='cd ~/projects/udeploy-aggregator'
+
+alias gdelpush='gpodb && gpsup'
+  
+alias ucmds='vim ~/.oh-my-zsh/plugins/uber/uber.plugin.zsh'
+#export DARTIUM_EXPIRATION_TIME=1577836800
