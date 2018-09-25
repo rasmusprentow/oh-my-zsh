639a5d72d32d879d57c2481179e1a43693772a5f
Rasmus Prentow <prentow@uber.com>
2018-09-25 09:00:23 +0200

    Added new command

--- plugins/uber/uber.plugin.zsh
+++ plugins/uber/uber.plugin.zsh
@@ -4,6 +4,7 @@ alias grim='git rebase -i master'
 alias grhs='git reset HEAD^1 --soft'
 alias gaca='git add -A && git commit --amend'
 alias gacan='git add -A && git commit --amend --no-edit'
+alias gctmp='git commit -A -m "temp"'
 
 # arc land keep branch. Lands the first commit in a stack
 alkb() {
@@ -26,13 +27,32 @@ adra() {
   COMMITS=$(git log --format=format:%H master...)
   while read -r ref
   do
+    ISTEMP=$(git log --pretty=oneline --abbrev-commit $ref | head -n 1 | grep 'temp$')
+    if [[ ! -z "$ISTEMP" ]]; then continue; fi
     git checkout $ref
     arc diff HEAD^1 -m 'rebase' --nolint --excuse 'Automated run on stacked diff, linting is too slow'
   done<<<$COMMITS
   git checkout $FEATURE_BRANCH
 }
 
-alias udc='cd ~/projects/developer-console; cn'
+# gcad = git commit arc diff
+# Commits all files and make a diff which includes a "Depends on" ref to the previous diff in the stack.
+# Usage: gcad <commit message>
+gcad() {
+  FEATURE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  git add -A && \
+  git commit -m "$*" && \
+  git push -fu origin $FEATURE_BRANCH && \
+
+  PREV_STACK_DIFF=$(git log HEAD^^1..HEAD^1 | grep "Differential Revision" | cut -d"/" -f 4)
+  MESSAGE_FILE=$(mktemp)
+  echo -e $* "\n\nSummary:\nDepends on" $PREV_STACK_DIFF > $MESSAGE_FILE
+
+  arc diff HEAD^1 --reviewers ,#si_ui_team --message-file $MESSAGE_FILE
+  rm $MESSAGE_FILE
+}
+
+alias udc='cd ~/projects/developer-console; nvm use'
 
 alias ucmds='vim ~/.oh-my-zsh/plugins/uber/uber.plugin.zsh'
 alias afix='gacan && adh -m "fix"'
